c3a8e05c3ed6d36294b460d900ca2e021b74d6ea
Charles Gichuki <sasagichuki@gmail.com>
2017-07-17 18:09:29 +0300

    sidebar

---
+++ js/sidenav.js
@@ -0,0 +1,17 @@
+function openNav(){
+	document.getElementById("mySidenav").style.width="100%";
+}
+
+function closeNav(){
+	document.getElementById("mySidenav").style.width="0";
+}
+//PUsh content
+function openNav(){
+	document.getElementById('mySidenav').style.width="100%";
+	document.getElementById("main").style.width="100%";
+}
+
+function closeNav(){
+	document.getElementById('mySidenav').style.width="0";
+	document.getElementById("main").style.marginLeft="0";
+}
