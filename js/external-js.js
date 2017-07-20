a67985219153fa3680e4d2e5e7b49b16a47613e3
Kamz <sirbertly@touchinspiration.com>
2017-07-20 08:07:15 +0300

    slider btns

--- js/external-js.js
+++ js/external-js.js
@@ -1,9 +1,10 @@
 $('.carousel').carousel({
-	interval: 90000
+	// interval: 90000
+  pause: "click"
 })
 
 // mySlider_Text
 $('#mySlide_Text')#mySlide_Text({
-	interval: 90000
+	// interval: 90000
 })
 
