939a60876724071ff334dfa76f9b51ae6f5998e8
Kamz <sirbertly@touchinspiration.com>
2017-07-21 13:27:09 +0300

    wall works

--- js/open-content.js
+++ js/open-content.js
@@ -1,20 +1,12 @@
 function openContent(evt, contentName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+  debugger
+  // Get all elements with class="tabcontent" and hide them
+  $(".tab-content").css('display','none')
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+  // Get all elements with class="tablinks" and remove the class "active"
+  $(".tab-link").removeClass('active');
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(contentName).style.display = "block";
-    evt.currentTarget.className += " active";
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(contentName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
