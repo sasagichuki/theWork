4089c1a4aec01f0a92dd94c4ea9c7ac200a5480e
Kamz <sirbertly@touchinspiration.com>
2017-07-24 13:02:21 +0300

    hamburger menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,4 +1,5 @@
 $('.menu-burger, .menu-items').on('click', function() {
+  $('body').addClass('noscroll');
   $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
   $('.menu-bg, .menu-items').toggleClass('hidden');
   $('.menu-burger').toggleClass('green-background');
